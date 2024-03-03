49085f7417a5487b4da7183af761e75508047150
Wowaxur <wowaxur@yandex.ru>
2024-03-03 18:49:18 +0300

    hw10 03.03

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,19 +2,21 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: LoadingStateType = initState, action: LoadingActionType): LoadingStateType => {
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return { ...state, isLoading: action.isLoading };
         default:
-            return state
+            return state;
     }
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+    type: 'CHANGE_LOADING';
+    isLoading: boolean;
 }
+
+type LoadingStateType = typeof initState;
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
